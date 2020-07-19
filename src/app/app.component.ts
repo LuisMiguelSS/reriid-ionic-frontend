ac89a27313b8f1a6be7a047ac76dee917fbbaef3
Luis Miguel Soto Sánchez <luismiguelsoto@jerez.es>
2020-07-19 16:25:56 +0100

    Update main base component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
+import { LanguageService } from './providers/language/language.service';
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, ToastController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AuthenticatorService } from './auth/authenticator.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +13,25 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public authService: AuthenticatorService,
+    private router: Router,
+    private languageService: LanguageService
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
+      this.statusBar.show(); // .styleDefault();
+      this.statusBar.backgroundColorByHexString('#ff6e7b');
       this.splashScreen.hide();
+
+      this.languageService.setInitialAppLanguage();
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,16 @@
+import { Camera } from '@ionic-native/camera/ngx';
+import { WebsocketService } from './providers/reriid/websocket/websocket.service';
+import { PostdetailService } from './providers/post/postdetail.service';
+import { ChatService } from './providers/chat/chat.service';
+import { AlertService } from './alert/alert.service';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { SqliteService } from './providers/database/sqlite.service';
+import { SQLite } from '@ionic-native/sqlite/ngx';
+import { SQLitePorter } from '@ionic-native/sqlite-porter/ngx';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -9,15 +19,53 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { IonicStorageModule } from '@ionic/storage';
+
+import { SanitizerService } from './providers/url/sanitizer.service';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
+}
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    IonicStorageModule.forRoot(),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient]
+      },
+      defaultLanguage: 'en'
+    }),
+  ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    SQLite,
+    SQLitePorter,
+    SqliteService,
+    Geolocation,
+    AlertService,
+    ChatService,
+    PostdetailService,
+    SanitizerService,
+    WebsocketService,
+    Camera
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: []
 })
 export class AppModule {}
